02d49c1ff6345a09e12c8b4943afd972a4f6108a
Takahiro Nakaoku <giraffe_rakugaki@yahoo.co.jp>
2015-08-22 16:10:17 +0900

    タイトル画面の完成

---
+++ Assets/Components/Menu/CameraFadeControl.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CameraFadeControl : MonoBehaviour 
+{
+	// Use this for initialization
+    [SerializeField] GameObject fadeOutUI;
+    private Image blackImg;
+    public float fadeSpeed= 0.5F;
+    public bool  isFadeIn = true;
+    
+    private float alpha= 1.0F;
+    private float fadeDir= -1;
+
+
+    void Awake()
+    {
+        GameObject ui = GameObject.Instantiate(fadeOutUI);
+        blackImg = ui.transform.FindChild("FadeOut").GetComponent<Image>();
+    }
+    
+    void Start ()
+    {
+            
+        if( isFadeIn)
+        {
+            alpha = 1;
+            fadeIn();
+        }
+        else
+        {
+            alpha = 1;
+            fadeOut();
+        }
+    }
+
+    void Update()
+    {
+        alpha += fadeDir * fadeSpeed * Time.deltaTime;
+        alpha = Mathf.Clamp01(alpha);
+
+        Color color = blackImg.color;
+        color.a = alpha;
+        blackImg.color = color;
+    }
+
+    public void fadeIn ()
+    {
+        fadeDir = -1;
+    }
+    
+    public void fadeOut ()
+    {
+        fadeDir = 1;
+    }
+    
+    void Disable ()
+    {
+        this.enabled = false;
+    }
+}
