232f0c07d5ed3809688b1a77e7f67dd466b45763
Yuhei Ichikawa <iyuhei1900@gmail.com>
2015-08-23 12:10:46 +0900

    エフェクトの追加

---
+++ Assets/Components/AutoDeleteMotion.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoDeleteMotion : StateMachineBehaviour
+{
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
+	{
+		if (1F <= stateInfo.normalizedTime) 
+		{
+			GameObject.Destroy( animator.gameObject);
+		}
+	}
+}
